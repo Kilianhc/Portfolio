5a7f35e0ee0561a40e7b60dd78245f614c405af2
Kilian H <kilianhc@hotmail.com>
2025-04-10 11:34:18 +0100

    AboutSection translated

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -17,7 +17,10 @@ const resources = {
       },
       about: {
         title: "About Me",
-        description: "Hi! I'm Kilian, a passionate web developer..."
+        description1: "I'm Kilian Daniel Hernández Chirino, passionate about web development and creating interactive applications and experiences. I love solving problems and exploring new technologies, improving my skills daily, with a work ethic based on discipline.",
+        description2: "My programming journey began in early 2023 when I decided to teach myself web development, initially through online courses. In September of that same year, I began my studies as a Higher Technician in Multiplatform Application Development at Universae, completing my studies in January 2025 after completing an internship at Higher Education Software SL.",
+        description3: "In January 2025, after completing MAD, I managed to enter the Las Palmas University Foundation's School of Programmers, which gave me the opportunity to complete the Full Stack Web Development Bootcamp with Ironhack, which I successfully completed in April 2025. In this Bootcamp, I carried out most of my projects, putting into them the knowledge acquired over these years and which I continue to acquire today, with additional training.",
+        description4: "I have experience in JavaScript (ES6+), HTML5, CSS3, and the React library. I also have experience using Material UI, Node.js, Express.js, MongoDB, and, to a lesser extent, Java and MySQL.",
       },
       footer: {
         text: "© 2025 Kilian D. Hernández Chirino. All rights reserved."
@@ -44,7 +47,10 @@ const resources = {
       },
       about: {
         title: "Sobre mí",
-        description: "¡Hola! Soy Kilian, un apasionado del desarrollo web..."
+        description1: "Soy Kilian Daniel Hernández Chirino, apasionado por el desarrollo web y la creación de aplicaciones y experiencias interactivas. Me encanta resolver problemas y explorar nuevas tecnologías mejorando así mis habilidades día a día, con una ética de trabajo basada en la disciplina.",
+        description2: "Mi experiencia en el mundo de la programación comienza a principios del año 2023 cuando decido aprender desarrollo web de manera autodidacta, al principio por medio de cursos en línea. En Septiembre de ese mismo año comienzo mis estudios como Técnico Superior en Desarrollo de Aplicaciones Multiplataforma en Universae, terminando dichos estudios en Enero de 2025 tras realizar las prácticas formativas en la empresa Higher Education Software SL.",
+        description3: "En enero de 2025 tras haber terminado DAM consigo entrar en la Escuela de Programadores de la Fundación Universitaria de Las Palmas, lo que me da la oportunidad de realizar el Bootcamp de Desarrollo Web Full Stack con Ironhack, el cual finalizó de manera satisfactoria en Abril de 2025. En este Bootcamp realizo la mayoría de mis proyectos poniendo en ellos el conocimiento adquirido durante estos años y que a día de hoy sigo adquiriendo, con formación complementaria.",
+        description4: "Tengo conocimientos en los lenguajes de programación JavaScript (Es6+), HTML5, CSS3 y manejo de librería React. Además también tengo experiencia en el uso de Material UI, Node.js, Express.js, MongoDB, y en menor medida del lenguaje de programación Java y MySQL."
       },
       footer: {
         text: "© 2025 Kilian D. Hernández Chirino. Todos los derechos reservados."
--- src/pages/AboutSection/AboutSection.jsx
+++ src/pages/AboutSection/AboutSection.jsx
@@ -49,19 +49,22 @@ const AboutSection = () => {
               <SeeMore
                 collapsedContent={
                   <Typography textAlign="justify" variant="body1" paragraph>
-                    Soy Kilian Daniel Hernández Chirino, apasionado por el desarrollo web y la creación de aplicaciones y experiencias interactivas. Me encanta resolver problemas y explorar nuevas tecnologías mejorando así mis habilidades día a día, con una ética de trabajo basada en la disciplina.
+                    {t("about.description1")}
                   </Typography>
                 }
                 expandedContent={
                   <>
                     <Typography textAlign="justify" variant="body1" paragraph>
-                      Mi experiencia en el mundo de la programación comienza a principios del año 2023 cuando decido aprender desarrollo web de manera autodidacta, al principio por medio de cursos en línea. En Septiembre de ese mismo año comienzo mis estudios como Técnico Superior en Desarrollo de Aplicaciones Multiplataforma en Universae, terminando dichos estudios en Enero de 2025 tras realizar las prácticas formativas en la empresa Higher Education Software SL.
+                    {t("about.description1")}
                     </Typography>
                     <Typography textAlign="justify" variant="body1" paragraph>
-                      En enero de 2025 tras haber terminado DAM consigo entrar en la Escuela de Programadores de la Fundación Universitaria de Las Palmas, lo que me da la oportunidad de realizar el Bootcamp de Desarrollo Web Full Stack con Ironhack, el cual finalizó de manera satisfactoria en Abril de 2025. En este Bootcamp realizo la mayoría de mis proyectos poniendo en ellos el conocimiento adquirido durante estos años y que a día de hoy sigo adquiriendo, con formación complementaria.
+                    {t("about.description2")}
                     </Typography>
                     <Typography textAlign="justify" variant="body1" paragraph>
-                      Tengo conocimientos en los lenguajes de programación JavaScript (Es6+), HTML5, CSS3 y manejo de librería React. Además también tengo experiencia en el uso de Material UI, Node.js, Express.js, MongoDB, y en menor medida del lenguaje de programación Java y MySQL.
+                    {t("about.description3")}
+                    </Typography>
+                    <Typography textAlign="justify" variant="body1" paragraph>
+                    {t("about.description4")}
                     </Typography>
                   </>
                 }
